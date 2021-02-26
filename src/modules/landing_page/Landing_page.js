1c7b86e4681a07a57b76aa36c9df254d3e4ad1ea
faizal kamaruddin <faizal.itech92@gmail.com>
2021-02-26 11:31:44 +0800

    update landing page

--- src/modules/landing_page/Landing_page.js
+++ src/modules/landing_page/Landing_page.js
@@ -2,8 +2,30 @@ import React from 'react'
 
 function Landing_page() {
     return (
-        <div>
-            <h1>Landing Page</h1>
+        <div className="landing">
+            <div className="landing-main">
+
+            </div>
+            <div className="landing-trailer">
+                {/* bede */}
+                {/* create component untuk section ni,
+                    sebab kt bahagian bawah ada section
+                    yang sama. so dengan component kita
+                    bole reused
+                */}
+            </div>
+            <div className="landing-download">
+
+            </div>
+            <div className="landing-trailer">
+                {/* bede */}
+            </div>
+            <div className="landing-question">
+
+            </div>
+            <div className="landing-footer">
+
+            </div>
         </div>
     )
 }
